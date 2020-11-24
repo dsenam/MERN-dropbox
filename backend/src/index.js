2cd1e1027ec00cc873e0570b6ff65cd92583d7e5
INDRA\dsenam <dsenam@indracompany.com>
2020-11-24 14:50:48 -0300

    + mongoDB mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express();
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.wtfcj.mongodb.net/dropbox?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
