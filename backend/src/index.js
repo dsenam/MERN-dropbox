35ffd29492278beaeac4775f70a14c0e727a0939
INDRA\dsenam <dsenam@indracompany.com>
2020-11-25 00:58:16 -0300

    + socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require("path")
+const cors = require('cors')
+
 const app = express();
+app.use(cors())
+
+
+//protocolos http e websocket
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box)
+    })
+})
 
 mongoose.connect('mongodb+srv://admin:admin@cluster0.wtfcj.mongodb.net/dropbox?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next()
+})
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -14,4 +35,5 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use(require('./routes'))
 
-app.listen(3333)
+//definição do web socket + http
+server.listen(3333)
