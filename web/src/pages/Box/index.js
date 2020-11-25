dc9388f8ef21946945993c846335e38fd5b93a12
INDRA\dsenam <dsenam@indracompany.com>
2020-11-25 05:15:55 -0300

    + requisição posts ok

--- web/src/pages/Box/index.js
+++ web/src/pages/Box/index.js
@@ -1,38 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {MdInsertDriveFile} from 'react-icons/md'
 import { Container } from './styles';
+import moment from 'moment'
+import api from '../../services/api'
+import {useParams} from 'react-router-dom'
+
 
 function Box() {
+    //recuperar dados dos parametros (id)
+    let {id} = useParams()
+    const [box, setBox] = useState([])
+
+    useEffect(()=> {
+        async function loadBox() {
+            
+            console.log(id)        
+            const response = await api.get(`boxes/${id}`)
+
+            setBox(response.data)
+        }
+        loadBox()
+        
+    }, [id, box])
+
   return (
     <Container>
         <header>
-            <h1>Titulo</h1>
+        <h1>{box.title}</h1>
         </header>
 
         <ul>
-            <li>
-                <a href=""> 
+            {box.files && box.files.map(file => (
+                <li>
+                <a href={file.url} target="blank"> 
                     <MdInsertDriveFile />
-                    <strong>Desafio.pdf</strong>
+                    <strong>{file.title}</strong>
                 </a>
-                <span>há 3 minutos atrás</span>     
+            <span>há {" "}{moment(file.createdAt).fromNow()}</span>     
             </li>
-
-            <li>
-                <a href=""> 
-                    <MdInsertDriveFile />
-                    <strong>Desafio.pdf</strong>
-                </a>
-                <span>há 3 minutos atrás</span>     
-            </li>
-
-            <li>
-                <a href=""> 
-                    <MdInsertDriveFile />
-                    <strong>Desafio.pdf</strong>
-                </a>
-                <span>há 3 minutos atrás</span>     
-            </li>
+            ))}
         </ul>
     </Container>
   );
